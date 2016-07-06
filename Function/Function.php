01f991e0d1699ab771afcc526bfa8e4a46830d8b
Dennis <delmore85@users.noreply.github.com>
2016-07-06 12:36:14 -0400

    Adding Function
    This is a php script that defines a function and passes parameters. Based on a Codecademy lesson.

---
+++ Function/Function.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body>
+      <p>
+        <?php
+        function aboutME($name, $age){
+            echo "Hello! My name is ".$name.", and I am ".$age." years old.";
+        }
+        aboutME("Dennis","30");
+        ?>
+      </p>
+    </body>
+</html>
